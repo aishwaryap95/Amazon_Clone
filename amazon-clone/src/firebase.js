263ebe77ff508d48b6e5b7ac10effcad5d352817
Aishwarya <aishwarya77769@gmail.com>
2023-03-31 11:48:11 +0530

    created react app and setting up firebase

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAaVOylo0W9RuOq8rnvx4fEzt2iiYqyIPE",
+  authDomain: "clone-challenge-e0a46.firebaseapp.com",
+  projectId: "clone-challenge-e0a46",
+  storageBucket: "clone-challenge-e0a46.appspot.com",
+  messagingSenderId: "897593323187",
+  appId: "1:897593323187:web:cdb6199e7fc99cd3ac0ac6",
+  measurementId: "G-VGV1RS9CHX"
+};
