90d7a3686fdb87bd797ab58af815c60c9037e239
Aishwarya <aishwarya77769@gmail.com>
2023-04-10 21:31:09 +0530

    done with create new amazon account

--- amazon-clone/src/firebase.js
+++ amazon-clone/src/firebase.js
@@ -1,4 +1,8 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import 'firebase/compat/auth';
+
 const firebaseConfig = {
   apiKey: "AIzaSyAaVOylo0W9RuOq8rnvx4fEzt2iiYqyIPE",
   authDomain: "clone-challenge-e0a46.firebaseapp.com",
@@ -6,5 +10,12 @@ const firebaseConfig = {
   storageBucket: "clone-challenge-e0a46.appspot.com",
   messagingSenderId: "897593323187",
   appId: "1:897593323187:web:cdb6199e7fc99cd3ac0ac6",
-  measurementId: "G-VGV1RS9CHX"
+  measurementId: "G-VGV1RS9CHX",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
--- amazon-clone/src/login.js
+++ amazon-clone/src/login.js
@@ -1,9 +1,41 @@
 //rfce
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 import { Link } from "react-router-dom";
+import { auth } from "./firebase";
+
+
+import { useNavigate } from "react-router-dom";
 
 function Login() {
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = (e) => {
+    e.preventDefault();
+
+    //firebase login - email-pswd enabled
+  };
+
+  const register = (e) => {
+    e.preventDefault();
+
+    //firebase register
+
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((auth) => {
+        //successfully created a new user with email
+        console.log(auth);
+        if (auth) {
+          navigate("/");
+        }
+      })
+      .catch((error) => alert(error.message));
+  };
+
   return (
     <div className="login">
       <Link to="/">
@@ -19,13 +51,22 @@ function Login() {
 
         <form>
           <h5>E-mail</h5>
-          <input type="text"></input>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
 
           <h5>Password</h5>
-          <input type="password"></input>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-          <button 
-          className="login_signInButton">Sign In</button>
+          <button type="submit" onClick={signIn} className="login_signInButton">
+            Sign In
+          </button>
         </form>
 
         <p>
@@ -34,7 +75,9 @@ function Login() {
           Interest-Based Ads Notice.
         </p>
 
-        <button className="login_registerButton"> Create your Amazon Account</button>
+        <button onClick={register} className="login_registerButton">
+          Create your Amazon Account
+        </button>
       </div>
     </div>
   );
